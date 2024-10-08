63f366263078ed3a78c7eef116c2d15d1f6a9bf4
Prakash Chhetri <prakashchhetri@Prakashs-MacBook-Air.local>
2024-10-08 19:32:30 +1300

    add: food item created

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,16 +1,27 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
-import StoreContextProvider from "../../context/storeContext";
+import FoodItem from "../FoodItem/FoodItem";
+import { StoreContext } from "../../context/storeContext";
 
 function FoodDisplay() {
-  const food_list = useContext(StoreContextProvider);
+  const { food_list } = useContext(StoreContext);
+  console.log("food list..", food_list);
 
   return (
     <div className='food-list' id='food-list'>
       <h2>Top dishes near you</h2>
       <div>
         {food_list.map((item, index) => {
-          <div></div>;
+          return (
+            <FoodItem
+              key={index}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              image={item.image}
+              price={item.price}
+            />
+          );
         })}
       </div>
     </div>
