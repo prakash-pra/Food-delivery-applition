043c190d26c544c5e09afe4c772c6fc637aa7d11
Prakash Chhetri <prakashchhetri@Prakashs-MacBook-Air.local>
2024-10-17 15:33:03 +1300

    add routes, add product input fields

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Sidebar from "./components/sidebar/sidebar";
-
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Order from "./pages/Orders/Order";
 const App = () => {
   return (
     <div>
@@ -9,6 +12,11 @@ const App = () => {
       <hr />
       <div className='app-content'>
         <Sidebar />
+        <Routes>
+          <Route path='/add' element={<Add />} />
+          <Route path='/list' element={<List />} />
+          <Route path='/order' element={<Order />} />
+        </Routes>
       </div>
     </div>
   );
---
+++ admin/src/pages/Orders/Order.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Order.css";
+
+function Order() {
+  return <div>Order</div>;
+}
+
+export default Order;
